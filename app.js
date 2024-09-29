efe99f8814fd8777eb01047712d79dea49a68788
Almas Murat <almasmurat05.kz@gmail.com>
2024-09-30 01:24:54 +0500

    Centered content!

--- app.js
+++ app.js
@@ -3,320 +3,320 @@ const contractAddress = "0x421ed83031559cD5387cC3F7074C83cBcF7e3D52";
 
 // Replace with your contract's ABI
 const contractABI = [
-    {
-      inputs: [],
-      stateMutability: "nonpayable",
-      type: "constructor",
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: false,
-          internalType: "address",
-          name: "owner",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "amount",
-          type: "uint256",
-        },
-      ],
-      name: "FundsWithdrawn",
-      type: "event",
-    },
-    {
-      inputs: [
-        {
-          internalType: "string",
-          name: "_name",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "_description",
-          type: "string",
-        },
-        {
-          internalType: "uint256",
-          name: "_price",
-          type: "uint256",
-        },
-      ],
-      name: "listModel",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "string",
-          name: "name",
-          type: "string",
-        },
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "price",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "address",
-          name: "creator",
-          type: "address",
-        },
-      ],
-      name: "ModelListed",
-      type: "event",
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "address",
-          name: "buyer",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "price",
-          type: "uint256",
-        },
-      ],
-      name: "ModelPurchased",
-      type: "event",
-    },
-    {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: false,
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          indexed: false,
-          internalType: "address",
-          name: "rater",
-          type: "address",
-        },
-        {
-          indexed: false,
-          internalType: "uint8",
-          name: "rating",
-          type: "uint8",
-        },
-      ],
-      name: "ModelRated",
-      type: "event",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "_modelId",
-          type: "uint256",
-        },
-      ],
-      name: "purchaseModel",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "_modelId",
-          type: "uint256",
-        },
-        {
-          internalType: "uint8",
-          name: "_rating",
-          type: "uint8",
-        },
-      ],
-      name: "rateModel",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "withdrawFunds",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "_modelId",
-          type: "uint256",
-        },
-      ],
-      name: "getModelDetails",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          internalType: "string",
-          name: "name",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "description",
-          type: "string",
-        },
-        {
-          internalType: "uint256",
-          name: "price",
-          type: "uint256",
-        },
-        {
-          internalType: "address",
-          name: "creator",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "averageRating",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "modelCount",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "models",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "id",
-          type: "uint256",
-        },
-        {
-          internalType: "string",
-          name: "name",
-          type: "string",
-        },
-        {
-          internalType: "string",
-          name: "description",
-          type: "string",
-        },
-        {
-          internalType: "uint256",
-          name: "price",
-          type: "uint256",
-        },
-        {
-          internalType: "address payable",
-          name: "creator",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "totalRating",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "ratingCount",
-          type: "uint256",
-        },
-        {
-          internalType: "bool",
-          name: "isPurchased",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "owner",
-      outputs: [
-        {
-          internalType: "address",
-          name: "",
-          type: "address",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "address",
-          name: "",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      name: "purchases",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "view",
-      type: "function",
-    }
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "FundsWithdrawn",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_price",
+        type: "uint256",
+      },
+    ],
+    name: "listModel",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+    ],
+    name: "ModelListed",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "buyer",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+    ],
+    name: "ModelPurchased",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "rater",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint8",
+        name: "rating",
+        type: "uint8",
+      },
+    ],
+    name: "ModelRated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_modelId",
+        type: "uint256",
+      },
+    ],
+    name: "purchaseModel",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_modelId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "_rating",
+        type: "uint8",
+      },
+    ],
+    name: "rateModel",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawFunds",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_modelId",
+        type: "uint256",
+      },
+    ],
+    name: "getModelDetails",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "averageRating",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "modelCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "models",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+      {
+        internalType: "address payable",
+        name: "creator",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "totalRating",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "ratingCount",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "isPurchased",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "purchases",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
 
 // Initialize Web3
@@ -351,12 +351,12 @@ document
     const name = document.getElementById("modelName").value;
     const description = document.getElementById("modelDescription").value;
     const priceEth = document.getElementById("modelPrice").value;
-		const priceWei = web3.utils.toWei(priceEth, "ether"); // "100000000000000000"
+    const priceWei = web3.utils.toWei(priceEth, "ether"); // "100000000000000000"
 
     try {
       await contract.methods
-  		.listModel(name, description, priceWei)
-  		.send({ from: window.account, gas: 300000});
+        .listModel(name, description, priceWei)
+        .send({ from: window.account, gas: 300000 });
       alert("Model listed successfully!");
       loadModels();
     } catch (error) {
@@ -400,7 +400,7 @@ async function purchaseModel(modelId) {
 
     await contract.methods
       .purchaseModel(modelId)
-      .send({ from: window.account, value: price, gas: 300000});
+      .send({ from: window.account, value: price, gas: 300000 });
     alert("Model purchased successfully!");
   } catch (error) {
     console.error(error);
@@ -419,7 +419,7 @@ async function rateModel(modelId) {
   try {
     await contract.methods
       .rateModel(modelId, rating)
-      .send({ from: window.account, gas: 300000});
+      .send({ from: window.account, gas: 300000 });
     alert("Model rated successfully!");
   } catch (error) {
     console.error(error);
@@ -442,7 +442,9 @@ async function viewDetails(modelId) {
 // Withdraw Funds (for contract owner)
 async function withdrawFunds() {
   try {
-    await contract.methods.withdrawFunds().send({ from: window.account, gas: 300000});
+    await contract.methods
+      .withdrawFunds()
+      .send({ from: window.account, gas: 300000 });
     alert("Funds withdrawn successfully!");
   } catch (error) {
     console.error(error);
